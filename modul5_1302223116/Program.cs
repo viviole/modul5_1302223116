a291afe1b630f04667ba23e565b966802484a9db
theodore <theodorelempas@gmail.com>
2024-03-23 09:04:46 +0700

    selesai

--- modul5_1302223116/Program.cs
+++ modul5_1302223116/Program.cs
@@ -9,15 +9,7 @@ internal class Program
         data.AddNewData(02);
         data.AddNewData(22);
         data.PrintAllData();
-    }
-}
-// See https://aka.ms/new-console-template for more information
-using modul5_1302223116;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
         Penjumlahan penjumlahan = new Penjumlahan();
         penjumlahan.jumlahTigaAngka<int>(13, 02, 22);
     }
